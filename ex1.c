b602ca6bdf712a912bbb6e4c1e6fcc8e40b47773
张础麒 <128697483+chuqi-zhang-vinci@users.noreply.github.com>
2023-10-23 12:58:08 +0200

    ex1 tp4

---
+++ ex1.c
@@ -0,0 +1,107 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+
+int main(int argc, char const *argv[])
+{
+	int hauteur, largeur;
+	printf("Veuillez entrer la hauteur : ");
+	scanf("%d", &hauteur);
+	printf("Veuillez entrer la largeur : ");
+	scanf("%d", &largeur);
+	printf("\n");
+
+
+	char **m;
+	m = (char**) malloc (hauteur * sizeof(char*));
+	if(m == NULL) exit(1);
+	for (int i = 0; i < hauteur; i++)
+	{
+		m[i] = (char*) malloc(largeur * sizeof(char));
+			for (int j = 0; j < largeur; j++){
+				m[i][j] = '.';
+			}
+		if(m[i] == NULL) exit(1);
+	}
+
+
+	printf("vous etes ici \n");
+	for (int i = 0; i < hauteur; i++)
+	{
+		for (int j = 0; j < largeur; j++)
+		{
+			printf("%c  ", m[i][j]);
+		}
+		printf("\n");
+	}
+
+
+	int colonneX = hauteur, colonneO = hauteur;
+	bool tourAX = true;
+	int compteurLigneX = hauteur-1;
+	int compteurLigneO = hauteur-1;
+		
+	while(colonneX != 0 && colonneO != 0) {	
+		printf("je suis ici : ");
+		printf("%d\n", tourAX);
+
+
+		printf("%d \n", compteurLigneX);
+		printf("%d \n", colonneX);
+		if(tourAX == true){
+			printf("la colonne où va jouer le joueur X : ");
+			scanf("%d", &colonneX);
+			colonneX -= 1;
+			if(m[compteurLigneX][colonneX] == '.'){
+				m[compteurLigneX][colonneX] = 'X';
+				compteurLigneX--;
+			}
+			else{
+				compteurLigneX--;
+				m[compteurLigneX][colonneX] = 'X';
+			}
+			tourAX = false;
+		}
+
+
+
+
+		printf("Affichage du tableau : \n");
+		for (int i = 0; i < hauteur; i++){
+			for (int j = 0; j < largeur; j++){
+				printf("%c  ", m[i][j]);
+			}
+			printf("\n");
+		}
+		
+		if(tourAX == false){
+			printf("la colonne où va jouer le joueur O : ");
+			scanf("%d", &colonneO);	
+			colonneO -= 1;
+			if(m[compteurLigneO][colonneO] == '.'){
+			m[compteurLigneO][colonneO] = 'O';
+			compteurLigneO--;
+			}
+			else{
+				compteurLigneO--;
+				m[compteurLigneO][colonneO] = 'O';
+			}
+			tourAX = true;
+		}
+		
+		printf("Affichage du tableau : \n");
+		for (int i = 0; i < hauteur; i++){
+			for (int j = 0; j < largeur; j++){
+				printf("%c  ", m[i][j]);
+			}
+			printf("\n");
+		}
+
+	}
+
+	for (int i = 0; i < hauteur; i++){
+			free(m[i]);
+		}
+		free(m);
+	return 0;
+}
